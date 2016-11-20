486a9df40789657f0be4e5c9694da97b965108f9
YiyiDeng <ivy112014@outlook.com>
2016-11-20 14:31:00 +0800

    updated

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,31 +1,70 @@
 //index.js
-//获取应用实例
+
+var util = require('../../utils/util.js')
 var app = getApp()
-Page( {
+Page({
   data: {
-    // 轮播
-    images: [
-      '../../images/local/d1.png',
-      '../../images/local/d2.png',
-      '../../images/local/d3.png',
-      '../../images/local/d4.png',
-      '../../images/local/d5.png'
-    ],
-    indicatorDots: true,  
-    autoplay: true,  
-    interval: 5000,  
-    duration: 1000,  
-    userInfo: {} 
+    feed: [],
+    feed_length: 0
   },
-  onLoad: function() {
-    console.log( 'onLoad' )
+  //事件处理函数
+  // 点击”分享“
+  bindItemTap: function() {
+    wx.navigateTo({
+      url: '../share/share'
+    })
+  },
+ 
+  onLoad: function () {
+    console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo( function( userInfo ) {
-      //更新数据
-      that.setData( {
-        userInfo: userInfo
-      })
-    })
+    this.refresh();
+  },
+  upper: function () {
+    wx.showNavigationBarLoading()
+    this.refresh();
+    console.log("upper");
+    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
+  },
+  lower: function (e) {
+    wx.showNavigationBarLoading();
+    var that = this;
+    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
+    console.log("lower")
+  },
+  
+  //网络请求数据, 实现首页刷新
+  refresh0: function(){
+    var index_api = '';
+    util.getData(index_api)
+        .then(function(data){
+          //this.setData({
+          //
+          //});
+          console.log(data);
+        });
+  },
+
+  //使用本地 fake 数据实现刷新效果
+  refresh: function(){
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+  },
+
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function(){
+    var next = util.getNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
   }
 })
