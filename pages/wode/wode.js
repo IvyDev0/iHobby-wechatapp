ce9a42e868284e46f4bc01447d65dc9a4da5b1fd
yiziyic <382052627@qq.com>
2016-11-27 13:05:06 +0800

    修改了一下正确显示wode

--- pages/wode/wode.js
+++ pages/wode/wode.js
@@ -2,37 +2,33 @@ var app = getApp()
 
 Page( {
   data: {
-    feed: [],
-    feed_length: 0,
     userInfo: {},
     userListInfo: [ {
-      icon: '../../images/wode/zan.png',
+      icon: '../../images/wode/yizan.png',
       text: '已赞',
 
     }, {
-        icon: '../../images/wode/iconfont-card.png',
+        icon: '../../images/wode/guanzhu.png',
         text:'关注',
        
       }, {
-        icon: '../../images/wode/iconfont-icontuan.png',
+        icon: '../../images/wode/fensi.png',
         text: '粉丝',
       }, {
-        icon: '../../images/wode/iconfont-shouhuodizhi.png',
+        icon: '../../images/wode/dingdan.png',
         text: '订单'
       }, {
-        icon: '../../images/wode/iconfont-kefu.png',
+        icon: '../../images/wodd/wenti.png',
         text: '问题'
       }]
   },
   onLoad: function() {
-    var that = this,
-    var f=util.getData().list;
+    var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo( function( userInfo ) {
       //更新数据
       that.setData( {
-      userInfo: userInfo,   
-      feed:f
+      userInfo: userInfo
     })
 
   })
