7fa5c960421cd38c614731be273adf5747420845
yiziyic <382052627@qq.com>
2016-11-27 22:49:41 +0800

    做信息页面

--- pages/xinxi/xinxi.js
+++ pages/xinxi/xinxi.js
@@ -1,31 +1,27 @@
-var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
-  data: {
-    toView: 'red',
-    scrollTop: 100
-  },
-  upper: function(e) {
-    console.log(e)
-  },
-  lower: function(e) {
-    console.log(e)
-  },
-  scroll: function(e) {
-    console.log(e)
-  },
-  tap: function(e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
+    data: {
+        inputShowed: false,
+        inputVal: ""
+    },
+    showInput: function () {
         this.setData({
-          toView: order[i + 1]
-        })
-        break
-      }
+            inputShowed: true
+        });
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        });
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        });
+    },
+    inputTyping: function (e) {
+        this.setData({
+            inputVal: e.detail.value
+        });
     }
-  },
-  tapMove: function(e) {
-    this.setData({
-      scrollTop: this.data.scrollTop + 10
-    })
-  }
-})
+});
