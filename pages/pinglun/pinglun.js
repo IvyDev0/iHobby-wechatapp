14640472b75a2b196fc4fffc42a5448ef631a171
YiyiDeng <ivy112014@outlook.com>
2016-11-28 13:10:47 +0800

    完成了“评论”，需要服务器真正实现功能

--- pages/pinglun/pinglun.js
+++ pages/pinglun/pinglun.js
@@ -2,13 +2,14 @@
 
 var util = require('../../utils/util.js')
 var app = getApp()
-var inputContent = {}
 
 Page({
   data: {
     feed:[],
-    inputContent: {}
+    inputContent: '',
+    userInfo: {}
   },
+  
   onLoad: function(e) {
     // 获取feed内容
     var id = e.feedid;
@@ -17,12 +18,31 @@ Page({
     this.setData({
       feed: feed_data[id-1]
     });
+    app.getUserInfo( function( userInfo ) {
+      that.setData( {
+      userInfo: userInfo
+      })
+    })
   },
 
-  bindChange: function(e) {
-    inputContent[e.currentTarget.id] = e.detail.value
+  // 获取输入评论
+  bindKeyInput: function(e) {
+    // 更新评论
+    this.setData({
+      inputContent: e.detail.value,
+      // 需要服务器。。。。。。。
+    })
+
   },
-  
+  // 点击回车收起键盘，录入输入数据
+  bindHideKeyboard: function(e) {
+    if (e.detail.value === '\n') {
+      //收起键盘
+      wx.hideKeyboard()
+    }
+    // 将this.data.inputContent加入数据库。。。。。。
+    
+  },
   // Event handler.
   dianzan: function(e) {
     // 对应num+1
@@ -40,5 +60,4 @@ Page({
       duration: 1500
     })
   },
-  
 })
